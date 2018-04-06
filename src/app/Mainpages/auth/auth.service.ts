381ad61a229694e814ce82ac90d3e2879b1316bf
HaHa21 <domlee2012@gmail.com>
2018-04-06 14:59:10 +0100

    Added comment for AuthService

--- src/app/Mainpages/auth/auth.service.ts
+++ src/app/Mainpages/auth/auth.service.ts
@@ -4,10 +4,11 @@ import { Http , Headers, Response } from "@angular/http";
 import 'rxjs/Rx';
 import { Observable } from "rxjs";
 import { User } from "./user.model";
+import { ErrorService } from "../../Components/errors/error.service";
 
 @Injectable()
 export class AuthService {
-    constructor(private http: Http) {}
+    constructor(private http: Http, private errorService: ErrorService) {}
 
     signup(user: User){
        console.log("Signup here");
@@ -15,7 +16,11 @@ export class AuthService {
        const headers = new Headers({'Content-Type': 'application/json'});
        return this.http.post('http://localhost:3000/user', body, {headers: headers})
             .map((response: Response) => response.json())
-            .catch((error: Response) => Observable.throw(error.json()));
+            .catch((error: Response) => {
+              //same as below
+              this.errorService.handleError(error.json());
+              return Observable.throw(error.json());
+            });
 
     }
 
@@ -24,12 +29,15 @@ export class AuthService {
       const headers = new Headers({'Content-Type': 'application/json'});
       return this.http.post('http://localhost:3000/user/signin', body, {headers: headers})
           .map((response: Response) => response.json())
-          .catch((error: Response) => Observable.throw(error.json()));
+          .catch((error: Response) => {
+            //throw backend error if does not meet condition
+            this.errorService.handleError(error.json());
+            return Observable.throw(error.json());
+          });
     }
 
     logout() {
       localStorage.clear();
     }
 
-    
 }
